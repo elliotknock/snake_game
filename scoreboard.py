3e3fe8ff8b69b0e3a3e72b6aa80e8408dc8a5026
Elliot <EKnock211@gmail.com>
2022-06-03 11:38:22 +0100

    Reads and Writes the high score to a file.

--- scoreboard.py
+++ scoreboard.py
@@ -2,12 +2,17 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ('Courier', 24, 'normal')
 
+file = open("data.txt")
+player_high_score = file.read()
+
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -16,15 +21,23 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         """Updates the scoreboard."""
-        self.write(f"Score: {self.score}", False, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", False, align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        """Shows a game over screen."""
-        self.goto(0, 0)
-        self.write(f"GAME OVER", False, align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     """Shows a game over screen."""
+    #     self.goto(0, 0)
+    #     self.write(f"GAME OVER", False, align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         """Increases the players score when food is picked up."""
         self.score += 1
-        self.clear()
         self.update_scoreboard()
